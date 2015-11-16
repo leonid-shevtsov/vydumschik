2ea937911ffeb6e88912755d11054e06f355f01a
Евгений Морозов <morozov@company.dnevnik.ru>
2015-11-16 16:13:10 +0300

    Add couple of tests in the same style

--- spec/lorem_spec.rb
+++ spec/lorem_spec.rb
@@ -14,4 +14,12 @@ describe Vydumschik::Lorem do
   it "should generate paragraphs" do
     subject.paragraph.should be_a String
   end
+
+  it "should generate text" do
+    subject.text(5).should be_a String
+  end
+
+  it "should generate text with expected size" do
+    subject.text(100).size.should eq 100
+  end
 end
